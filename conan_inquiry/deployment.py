dae04b6c28b38d08409b065b88203f4c68631390
Jan Dalheimer <jan@dalheimer.de>
2017-11-14 13:58:45 +0100

    Slightly improve the deployment script

--- conan_inquiry/deployment.py
+++ conan_inquiry/deployment.py
@@ -7,7 +7,7 @@ import shutil
 
 def deploy():
     with TemporaryDirectory() as dir:
-        print(dir)
+        print('* Deploying with tempdir:', dir)
 
         def git(subcmd, *args, **kwargs):
             kwargs.setdefault('cwd', os.path.join(dir, 'conan_inquiry'))
@@ -17,8 +17,10 @@ def deploy():
             if retcode != 0:
                 raise ChildProcessError('Unable to run the command')
 
-        git('clone', os.getenv('GITHUB_REPO', 'git@github.com:02JanDal/conan_inquiry.git'), cwd=dir)
-        git('checkout', 'gh-pages')
+        git('clone', os.getenv('GITHUB_REPO', 'git@github.com:02JanDal/conan_inquiry.git'),
+            '--branch', 'gh-pages',
+            '--single-branch',
+            cwd=dir)
 
         webpath = os.path.join(os.path.dirname(__file__), 'data/web')
         for file in ['packages.json', 'packages.js', webpath + '/index.html', webpath + '/script.js']:
