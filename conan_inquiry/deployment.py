ee19302003d5bcca8eb36a8f68c339231ed46410
Jan Dalheimer <jan@dalheimer.de>
2018-03-31 14:20:11 +0200

    Cache buster for script.js and packages.json

--- conan_inquiry/deployment.py
+++ conan_inquiry/deployment.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from tempfile import TemporaryDirectory
 
 import os
 import subprocess
 import shutil
+import hashlib
 
 
 def deploy():
@@ -26,6 +28,16 @@ def deploy():
         for file in ['packages.json', 'packages.js', webpath + '/index.html', webpath + '/script.js']:
             shutil.copy(file, os.path.join(dir, 'conan_inquiry'))
             git('add', os.path.join(dir, 'conan_inquiry', os.path.basename(file)))
+
+        index_html_name = os.path.join(dir, 'conan_inquiry', 'index.html')
+        with open(index_html_name, 'rt') as file:
+            index_html_data = file.read()
+        index_html_data = index_html_data.replace('CACHE_BUSTER',
+                                                  hashlib.md5(datetime.now().isoformat().encode('utf-8')).hexdigest())
+        print(index_html_data)
+        with open(index_html_name, 'wt') as file:
+            file.write(index_html_data)
+        git('add', index_html_name)
 
         git('commit', '--amend', '-m', '"Automatic deployment"')
         git('push', '-f', 'origin', 'gh-pages')
